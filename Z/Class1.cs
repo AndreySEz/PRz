297139ba08587e96585f4ec6800bf95a923cd869
Новиков Дмитрий <chelovek1214@gmail.com>
2020-12-04 18:30:48 +0500

    F

---
+++ Z/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z
+{
+    public class ClassOne
+    {
+        int per1 = 5;
+        int per2 = 50;
+
+        public void Out()
+        {
+            System.Windows.MessageBox.Show($"Новые значения: {per1}, {per2}");
+        }
+
+        public void Change(int oneArgue, int twoArgue)
+        {
+            per1 = oneArgue;
+            per2 = twoArgue;
+        }
+
+        int Sum()
+        {
+            int sum = per1 + per2;
+            return sum;
+        }
+
+        int Greatest()
+        {
+            return per1>per2?per1:per2; //Не совсем понятно, что нужно возвращать, если они одинаковые, посему пусть выводится значение обоих
+        }
+    }
+}
---
+++ Z/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Z
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        ClassOne one = new ClassOne();
+
+        private void button_click(object sender, RoutedEventArgs e)
+        {
+            string[] strings=textBox.Text.Split(' ');
+            int[] ints = new int[strings.Length];
+            {
+                int iteration = 0;
+                foreach (string g in strings)
+                {
+                    ints[iteration] = Convert.ToInt32(strings[iteration]);
+                    iteration++;
+                }
+            }
+            one.Change(ints[0], ints[1]);
+            one.Out();
+        }
+    }
+}
